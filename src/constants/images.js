60dd9d777b3e16dbac47b22d1291d126e448b98e
minhthuan231000 <minhthuan23100@gmail.com>
2021-10-01 12:39:05 +0700

    Updates

--- src/constants/images.js
+++
@@ -1,8 +0,0 @@
-import banner1 from '../assets/images/Banner1.jpg'
-import banner2 from '../assets/images/Banner2.jpg'
-
-const Images = {
-    BANNER1: banner1,
-    BANNER2: banner2,
-}
-export default Images;
